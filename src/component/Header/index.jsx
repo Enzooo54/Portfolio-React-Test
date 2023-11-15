921070f32a3eb1b8b64b6750500459521adefb04
Enzooo54 <enzo.sontot88@gmail.com>
2023-11-15 19:52:06 +0100

    Update portfolio page content and remove unnecessary files

---
+++ src/component/Header/index.jsx
@@ -0,0 +1,25 @@
+import "./Header.css";
+import React from 'react';
+import Photo from "../../assets/Enzoleiller.png";
+
+function Header() {
+    return (
+        <nav>
+            <ul className="logo">
+                <li className="logo"><img src={Photo} alt="logo"/></li>
+                <li className="enzo"><a href="/">Enzo</a></li>
+            </ul>
+            <ul>
+                <li><a href="/a-propos">à propos</a></li>
+                <li><a href="/service">Services</a></li>
+                <li><a href="/prix">Prix</a></li>
+                <li><a href="/soon">Blog</a></li>
+            </ul>
+            <ul>
+                <li className="contact"><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
+        )
+}
+
+export default Header;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div style={{width: "auto", height:"90vh", backgroundColor: '#28293E', margin: 0}}>
+            <h1 style={{color: "#FFF", fontSize: "72px", fontStyle: "normal", fontWeight: 800, lineHeight: "80px", letterSpacing: -2, margin: 0}}>The Simple, lean Design</h1>
+        </div>
+    );
+}
+
+export default App;
